9f3fd5df5c6f5b81a20e4ce601e60765d4df1cec
cpashp <cpelus1@students.towson.edu>
2018-02-14 21:12:50 -0500

    eliminated type checking smells using JDeodorant

--- src/edu/towson/cis/cosc442/project1/monopoly/RailRoadCell.java
+++ src/edu/towson/cis/cosc442/project1/monopoly/RailRoadCell.java
@@ -35,4 +35,14 @@ public class RailRoadCell extends Cell {
 			currentPlayer.payRentTo(theOwner, getRent());
 		}
 	}
+
+	public void playerMoved(Player player, int playerIndex, GameMaster gameMaster) {
+		if (this.isAvailable()) {
+			int price = this.getPrice();
+			if (price <= player.getMoney() && price > 0) {
+				gameMaster.getGUI().enablePurchaseBtn(playerIndex);
+			}
+		}
+		gameMaster.getGUI().enableEndTurnBtn(playerIndex);
+	}
 }
--- src/edu/towson/cis/cosc442/project1/monopoly/UtilityCell.java
+++ src/edu/towson/cis/cosc442/project1/monopoly/UtilityCell.java
@@ -38,4 +38,14 @@ public class UtilityCell extends Cell {
 			currentPlayer.payRentTo(theOwner, getRent(diceRoll));
 		}
 	}
+
+	public void playerMoved(Player player, int playerIndex, GameMaster gameMaster) {
+		if (this.isAvailable()) {
+			int price = this.getPrice();
+			if (price <= player.getMoney() && price > 0) {
+				gameMaster.getGUI().enablePurchaseBtn(playerIndex);
+			}
+		}
+		gameMaster.getGUI().enableEndTurnBtn(playerIndex);
+	}
 }
